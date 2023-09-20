b71cf7d07e06e59704eaaab63c2cb9a648720d23
ViTBaR199 <barantsev.vitalik@mail.ru>
2023-09-20 16:44:22 +0300

    Removed the outputs to the console

--- Сryptography.cpp
+++ Сryptography.cpp
@@ -1,34 +1,29 @@
 ﻿#include <iostream>
 
 //первый номер
-void P_block(unsigned char* arr1, unsigned char* arr2, const int size_perm) {
+unsigned char* P_block(unsigned char* arr1, unsigned char* arr2, const int size_perm) {
+    unsigned char* new_arr = new unsigned char[size_perm];
     for (int i = 0; i < size_perm; i++) {
         int index = arr2[i] - '0';
         char temp = arr1[i];
-        arr2[i] = arr1[index];
-
-        std::cout << arr2[i];
+        new_arr[i] = arr1[index];
     }
-    std::cout << "\n";
+    return new_arr;
 }
 
 //второй номер
-void S_block(unsigned char* arr1, unsigned char* arr_k, const char size_k) {
+unsigned char* S_block(unsigned char* arr1, unsigned char* arr_k, const char size_k) {
     //замена идёт по всему массиву справа налево
+    unsigned char* new_arr = new unsigned char[sizeof(arr1)];
     for (int i = sizeof(arr1) - 1; i >= 0;)
     {
         for (int j = (size_k - '0') - 1; j >= 0 && i >= 0; j--)
         {
-            arr1[i] = arr_k[j];
+            new_arr[i] = arr_k[j];
             i--;
         }
     }
-
-    for (int i = 0; i < sizeof(arr1); i++)
-    {
-        std::cout << arr1[i];
-    }
-
+    return new_arr;
 }
 
 
@@ -43,9 +38,19 @@ int main()
     char k = '3';
     unsigned char arr_k[] = { '0', '1', '0' };
 
-    P_block(arr1, arr2, size);   
-    std::cout << '\n';
-    S_block(arr1, arr_k, k);
-    
-    return 0;
+    //проверка первой функции
+    unsigned char* last_check = P_block(arr1, arr2, size);
+    for (int i = 0; i < sizeof(last_check); i++)
+    {
+        std::cout << last_check[i];
+    }
+    std::cout << std::endl;
+
+    //проверка второй функции
+    unsigned char* ultra_last_check = S_block(arr1, arr_k, k);
+    for (int i = 0; i < sizeof(last_check); i++)
+    {
+        std::cout << ultra_last_check[i];
+    }
+
 }
